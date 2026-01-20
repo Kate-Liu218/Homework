cdeb41cc3541e65ccc5965ca732cff593d2ff19d
unknown <Kate-Liu218>
2026-01-20 14:46:40 +0800

    首次提交：完成交互功能开发

---
+++ Homework/interpersonal greeting.py
@@ -0,0 +1,19 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您自己的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+while True:
+    user_input = input("请输入: ")
+    if user_input.lower() == "hello":
+        print("Hello, world!")
+        break
+    else:
+        print("输入不正确")
